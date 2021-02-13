ee30381bc02d3575c23216ece027fd147d46f9ca
NC <chauhannabin90@gmail.com>
2021-02-13 19:27:33 +0545

    basic changes.

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-const fs = require('fs');
-const _ = require('lodash');
-const express = require('express');
+const fs = require("fs");
+const _ = require("lodash");
+const express = require("express");
 
 const app = express();
-
+app.set("view engine", "ejs");
 //listen for requests.
 app.listen(5206);
 
-//routing
-app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
-})
-app.get('/about',(req,res)=>{
-    res.sendFile('./views/about.html',{root:__dirname});
-})
-//redirects
-app.get('/home',(req,res)=>{
-    res.redirect('/');
-})
-app.get('/about-old',(req,res)=>{
-    res.redirect('/about')
-})
-
-//wildcard/404
-
-app.use((req,res)=>{
-    console.log(__dirname);
-    res.sendFile('./views/404.html', {root:__dirname})
-})
+//routings
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+app.get("/about", (req, res) => {
+    res.render("about",{title:'About'});
+});
+app.get("/articles", (req, res) => {
+    res.render("articles",{title:'Articles'});
+});
+app.get("/add-article", (req, res) => {
+    res.render("add-article",{title:'Add Article'});
+});
+//wildcard/404 redirect
+app.use((req, res) => {
+    res.status(404).render("404",{title:'404 Not Found'});
+});
